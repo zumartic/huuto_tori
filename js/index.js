6f996e3f24f38fcb50c4e930c7bc002b779b7464
Sami Kangas <zumartic@gmail.com>
2016-10-04 13:44:25 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 function resultSearch() {
-   $("#huuto").html('<embed src="https://cors-anywhere.herokuapp.com/http://www.huuto.net/hakutulos?words=' +$("#fleeSearch").val()  +'&area=" width="900" height="800">');
-  $("#tori").html('<embed src="https://cors-anywhere.herokuapp.com/http://www.tori.fi/koko_suomi?q=' +$("#fleeSearch").val()  +'&cg=0&w=3" width="900" height="800">');
+   $("#huuto").html('<embed src="http://www.huuto.net/hakutulos?words=' +$("#fleeSearch").val()  +'&area=" width="900" height="800">');
+  $("#tori").html('<embed src="http://www.tori.fi/koko_suomi?q=' +$("#fleeSearch").val()  +'&cg=0&w=3" width="900" height="800">');
 }
